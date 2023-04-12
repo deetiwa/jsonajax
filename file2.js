cec9872f5cde971bfa288be20c16ab6f196e0473
Ravi Tiwari <ravitiwari254@gmail.com>
2023-04-12 16:11:55 +0530

    update file2 & index.html

--- file2.js
+++ file2.js
@@ -3,14 +3,21 @@
 
 
 
-// let student = {
-//     name: "Ankit",
-//     age: 20
-// };
+let student = {
+    name: "Ankit",
+    age: 20
+};
 
-//  let ans = JSON.stringify(student); // convert javascript value to javascript object notation.
-//  console.log(ans);
+ let ans = JSON.stringify(student); // convert javascript value to javascript object notation.
+ console.log(ans);
 
-//  let str = '{"name":"Ankit","age":20}';
-//  let ob = JSON.parse(str);             //convert javascript object notation string into an object.
-//  console.log(ob);
+ let str = '{"name":"Ankit","age":20}';
+ let ob = JSON.parse(str);             //convert javascript object notation string into an object.
+ console.log(ob);
+
+ function loadaboutinformation()       // call to ajax
+{
+   var request=new XMLHttpRequest()   // make object and call open fun^
+   request.open("GET", "file2.js"); // make open fun^ pass method
+   request.send();
+}
